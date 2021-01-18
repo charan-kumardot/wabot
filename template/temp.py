072bb78964420c96a8360e43263ef88c1a6d76ed
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2021-01-18 11:45:07 +0530

    Add files via upload

---
+++ template/temp.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+os.chdir(THIS_FOLDER)
+myFiles = glob.glob('*.txt')
+myFiles.sort()
+mas=[]
+for i in myFiles:
+    with open(os.path.join(THIS_FOLDER, i),'rb') as f:
+        mas.append(f.read())
+            
+        
+        
+        
+        
+        
+Template=[i for i in mas]
+Count=len(Template)
