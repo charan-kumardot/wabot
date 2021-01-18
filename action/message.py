19576b95eff41007af471f4ccbe0b769529f7dfa
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2021-01-18 11:43:33 +0530

    Delete message.py

--- action/message.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-os.chdir(THIS_FOLDER)
-myFiles = glob.glob('*.txt')
-myFiles.sort()
-
-Messages=[open(os.path.join(THIS_FOLDER, i),'r').read() for i in myFiles]    
-count=len(Messages)
